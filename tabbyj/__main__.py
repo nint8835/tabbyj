e2ade5d8f50b7df0842d298966cca5ef571f0449
Riley Flynn <riley@rileyflynn.me>
2019-06-27 19:00:38 -0230

    Fixed representation output and added coloured output

--- tabbyj/__main__.py
+++ tabbyj/__main__.py
@@ -4,21 +4,39 @@ from typing import Dict, Any, List
 import click
 import ujson as json
 
-def process_value(data: Any, *, location: str="") -> None:
+
+def get_json_repr(obj: Any) -> str:
+    if obj is None:
+        return click.style("null", fg="red")
+    elif isinstance(obj, bool):
+        return click.style(repr(obj).lower(), fg="red")
+    elif isinstance(obj, str):
+        return click.style(repr(obj), fg="cyan")
+    else:
+        return click.style(repr(obj), fg="yellow")
+
+
+def process_value(data: Any, *, location: str = "") -> None:
     if isinstance(data, dict):
         process_dict(data, location)
     elif isinstance(data, list):
         process_list(data, location)
     else:
-        print(f"{location} = {repr(data)}")
+        click.echo(f"{location} = {get_json_repr(data)}")
+
 
 def process_dict(data: Dict[str, Any], location: str) -> None:
     for key, value in data.items():
         process_value(value, location=f"{location}.{key}")
 
+
 def process_list(data: List[Any], location: str) -> None:
     for index, value in enumerate(data):
-        process_value(value, location=f"{location}[{index}]")
+        process_value(
+            value,
+            location=f"{location}[{click.style(str(index), fg='yellow')}]"
+        )
+
 
 @click.command()
 def tabbyj():
